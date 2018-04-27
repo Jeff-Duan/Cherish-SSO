30e82554db05dc54a98fcece417bd5c047ede10e
cherish-Hui <1191837698@qq.com>
2018-04-28 00:19:10 +0800

    SSO-Server完成单点注销
    N/A
    
    Signed-off-by: cherish-Hui <1191837698@qq.com>

--- src/main/java/com/cherish/demo/controller/SSOController.java
+++ src/main/java/com/cherish/demo/controller/SSOController.java
@@ -9,32 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/sso/")
 public class SSOController {
-
-    private Map result(int code, String status, String message) {
-        Map<String, Object> result = new HashMap<>();
-        result.put("code", code);
-        result.put("status", status);
-        result.put("message", message);
-        return result;
-    }
-
-    private Map loginWaitResult() {
-        return result(-1, "wait", "准备登陆。");
-    }
-
-    private Map loginSuccessResult() {
-        return result(0, "success", "登陆成功。");
-    }
-
-    private Map loginFailResult() {
-        return result(1, "fail", "登陆失败,用户名或密码错误。");
-    }
 
     @Autowired
     SSOService SSOService;
@@ -61,13 +39,13 @@ public class SSOController {
     public String logout(HttpSession session) {
         String callBackUrl = (String) session.getAttribute("CallBackUrl");
         session.invalidate();
-        return "redirect:/login?callBackUrl" + callBackUrl;
+        return "redirect:/sso/login?callBackUrl=" + callBackUrl;
     }
 
     @GetMapping(value = "/callBack")
     public String callback(HttpSession session) {
         String callBackUrl = (String) session.getAttribute("CallBackUrl");
-        return "redirect:/" + callBackUrl;
+        return "redirect:" + callBackUrl;
     }
 
 }
--- src/main/java/com/cherish/demo/filter/SSOFilter.java
+++ src/main/java/com/cherish/demo/filter/SSOFilter.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,7 +16,7 @@ public class SSOFilter implements Filter {
     private static final Logger logger = LoggerFactory.getLogger(SSOFilter.class);
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
 
     }
 
@@ -40,33 +39,34 @@ public class SSOFilter implements Filter {
         Optional<String> userOptional = Optional.ofNullable((String) session.getAttribute("User"));
         //获取Http请求的URL
         String requestURI = httpServletRequest.getRequestURI();
-
-        //放行登陆
-        if ("/sso/save".equals(requestURI)) {
-            chain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-        //未登陆回调SSO-Client(500)
-        if (StringUtils.isEmpty(userOptional.orElse(null)) && "/sso/logout".equals(requestURI)) {
-            httpServletResponse.sendError(500, "请勿在未登陆状态访问注销,属于非法访问!!!!!!!!!!!!!!!!!!!!!!!!!!!!!.");
-            return;
-        }
-        //未登陆回调SSO-Client(500)
-        if (StringUtils.isEmpty(userOptional.orElse(null)) && "/sso/callBack".equals(requestURI)) {
-            httpServletResponse.sendError(500, "请勿越过SSO直接回调SSO-Client,这将会导致无法获取SSO-Client的URL.");
-            return;
-        }
-        //不存在SSO-Client回调地址(500)
+        //SSO-Client回调地址不存在[非法]
         if (!callBackOptional.isPresent()) {
             httpServletResponse.sendError(500, "请勿越过SSO-Client直接登陆SSO,这将会导致无法获取SSO-Client的URL.");
             return;
         }
-        //若已登录,回调SSO-Client地址
-        if (!StringUtils.isEmpty(userOptional.orElse(null))) {
-
-            logger.info("Jsession:"+httpServletRequest.getRequestedSessionId());
-
-            httpServletResponse.sendRedirect((String) session.getAttribute("CallBackUrl"));
+        //提交登陆[无条件]
+        if ("/sso/save".equals(requestURI)) {
+            chain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        //注销[未登录-非法]
+        if (StringUtils.isEmpty(userOptional.orElse(null)) && "/sso/logout".equals(requestURI)) {
+            httpServletResponse.sendError(500, "请勿在未登陆状态访问注销,属于非法访问!!!!!!!!!!!!!!!!!!!!!!!!!!!!!.");
+            return;
+        }
+        //注销[已登录-放行]
+        if (!StringUtils.isEmpty(userOptional.orElse(null)) && "/sso/logout".equals(requestURI)) {
+            chain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        //回调SSO-Client[未登陆-非法]
+        if (StringUtils.isEmpty(userOptional.orElse(null)) && "/sso/callBack".equals(requestURI)) {
+            httpServletResponse.sendError(500, "请勿越过SSO直接回调SSO-Client,这将会导致无法获取SSO-Client的URL.");
+            return;
+        }
+        //回调SSO-Client[登陆-放行]
+        if (!StringUtils.isEmpty(userOptional.orElse(null)) && "/sso/callBack".equals(requestURI)) {
+            chain.doFilter(servletRequest, servletResponse);
             return;
         }
         logger.info("SSO-Client正在尝试登陆SSO.");
